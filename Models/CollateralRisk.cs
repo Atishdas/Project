b9876547b408634d662dbcced149cee9f2ceddb4
unknown <dasatish100@gmail.com>
2021-10-31 17:39:54 +0530

    first commit

---
+++ Models/CollateralRisk.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RiskAssessmentMicroservice.Models
+{
+    public class CollateralRisk
+    {
+        public int CollateralId { get; set; }
+        public double PercentageRisk { get; set; }
+        public DateTime DateAssessed
+        {
+            get { return DateTime.Now; }
+        }
+        public double MarketValue { get; set; }
+        public double SanctionedLoan { get; set; }
+    }
+}
---
+++ Repositories/RiskAssessmentRepository.cs
@@ -0,0 +1,53 @@
+﻿using RiskAssessmentMicroservice.Models;
+using System;
+using System.Net.Http;
+using Newtonsoft.Json;
+using Microsoft.Extensions.Configuration;
+
+namespace RiskAssessmentMicroservice.Repositories
+{
+    public class RiskAssessmentRepository : IRiskAssessmentRepository
+    {
+        static readonly log4net.ILog _log4net = log4net.LogManager.GetLogger(typeof(RiskAssessmentRepository));
+        private IConfiguration Configuration { get; }
+        public RiskAssessmentRepository(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+        public DetailCashDeposit GetCollateralCashDeposit(int loanId)
+        {
+            _log4net.Info("GetCollateralCashDeposit method under RiskAssessmentRepository accessed for processing loanId=" + loanId);
+            HttpClientHandler clientHandler = new HttpClientHandler();
+            using (HttpClient client = new HttpClient(clientHandler))
+            {
+                client.BaseAddress = new Uri(Configuration["ApiUrl:CollateralManagement"]);
+                HttpResponseMessage response = client.GetAsync("CollateralManagement/getCollateralsCashDeposit?loanId=" + loanId).Result;
+                if (response.IsSuccessStatusCode)
+                {
+                    string stringData = response.Content.ReadAsStringAsync().Result;
+                    return JsonConvert.DeserializeObject<DetailCashDeposit>(stringData);
+                }
+                else 
+                    return null;
+            }
+        }
+
+        public DetailRealEstate GetCollateralRealEstate(int loanId)
+        {
+            _log4net.Info("GetCollateralRealEstate method under RiskAssessmentRepository accessed for processing loanId=" + loanId);
+            HttpClientHandler clientHandler = new HttpClientHandler();
+            using (HttpClient client = new HttpClient(clientHandler))
+            {
+                client.BaseAddress = new Uri(Configuration["ApiUrl:CollateralManagement"]);
+                HttpResponseMessage response = client.GetAsync("CollateralManagement/getCollateralsRealEstate?loanId=" + loanId).Result;
+                if (response.IsSuccessStatusCode)
+                {
+                    string stringData = response.Content.ReadAsStringAsync().Result;
+                    return JsonConvert.DeserializeObject<DetailRealEstate>(stringData);
+                }
+                else
+                    return null;
+            }
+        }
+    }
+}
